125f2eda3d3c526f2f0ad158243dc353f9d56d14
Lvwenchao <giswenchao@gmail.com>
2018-12-04 11:34:13 +0800

    v1.0 first commit

---
+++ src/main/java/main/java/com/mmall/vo/source.java
@@ -0,0 +1,13 @@
+package main.java.com.mmall.vo;
+
+/**
+ * @author:
+ * @data: 2018/12/3 23:08
+ * @description:
+ * @Version: 1.0
+ **/
+public class source{
+    public static void main(String[] args) {
+
+    }
+}
